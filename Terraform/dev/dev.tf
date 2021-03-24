e340ad7b1bff3a467e10c4009a72a20dcea2eb46
sullub <58630511+sullub@users.noreply.github.com>
2021-03-24 10:38:05 +0000

    Update dev.tf

--- Terraform/dev/dev.tf
+++ Terraform/dev/dev.tf
@@ -57,6 +57,7 @@ resource "azurerm_log_analytics_workspace" "insights" {
   resource_group_name = local.full_rg_name
   sku                 = "PerGB2018"
   retention_in_days   = 30
+  depends_on = [local.full_rg_name]
 
    tags = {
     environment = terraform.workspace
